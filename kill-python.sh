6435612e84ba21063b0c491d4234c052654e235e
dksipl <dhruv.samyak45@gmail.com>
2022-09-02 07:37:04 +0530

    added gpu usage and log file

--- kill-python.sh
+++ kill-python.sh
@@ -1,14 +1,36 @@
+datee=`date +%F`
+timee=`date +%T`
+
+ip=`whoami`
+
+if [ ! -d "$HOME/GPU_temperature_logs" ]
+then
+	mkdir $HOME/GPU_temperature_logs
+fi
+
+filename=$HOME/GPU_temperature_logs/${ip}_${datee}_${timee}
+touch $filename
+
+who >> $filename
+date >> $filename
+
+echo " " >> $filename
+echo "time | GPU temperature | GPU memory usage" >> $filename
+
 while true
 do
 	temp=`nvidia-smi | grep 'MiB / ' | cut -d ' ' -f 5 | cut -d C -f 1`
+	usage=`nvidia-smi | grep 'MiB / ' | cut -d '|' -f 3 | cut -d '/' -f 1`
 
-	clear
-	echo GPU temperature: $temp C
+	echo "GPU temperature: $temp C		Usage: $usage"
 
-	if [[ $temp -ge 60 ]]
+	timee=`date +%T`
+	echo "$timee | $temp | $usage" >> $filename
+
+	if [[ $temp -ge 50 ]]
 	then
-		echo temperature reached above 60
-		echo kiling python
+		echo "temperature reached above 50" >> $filename
+		echo "kiling python..." >> $filename
 
 		id=`nvidia-smi | grep python | cut -d ' ' -f 15`
 
@@ -19,9 +41,12 @@ do
 
 		kill -9 $id
 
-		noti -t 'GPU temperature reached limit' -m 'killing python'
-		noti -t 'GPU temperature reached limit' -m 'killing python' --telegram
-		noti -t 'GPU temperature reached limit' -m 'killing python' -s
+		noti -t 'GPU temperature reached limit: $temp C' -m 'killed python'
+		noti -t 'GPU temperature reached limit: $temp C' -m 'killed python' --telegram
+		noti -t 'GPU temperature reached limit' -m 'killed python' -s
+
+		echo "killed python process" >> $filename
+		echo "done" >> $filename
 
 		break
 	fi
