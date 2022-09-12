f87b33820b831aea0dba36699aabaf738b04deb6
KD <59188880+KD1903@users.noreply.github.com>
2022-09-12 13:11:44 +0530

    Update kill-python.sh
    added all users ip in log file and telegram notification

--- kill-python.sh
+++ kill-python.sh
@@ -3,15 +3,30 @@ timee=`date +%T`
 
 ip=`whoami`
 
+max=10
+all_users=""
+
+for i in `seq 0 $max`
+do
+        temp=`who | grep "/$i"`
+        #echo $temp
+        all_users+="${temp: -3}"
+        #echo $all_users
+done
+
+echo $all_users
+
+noti -t "training stared on GPU" -m "logged in users: $all_users" --telegram
+
 if [ ! -d "$HOME/GPU_temperature_logs" ]
 then
-	mkdir $HOME/GPU_temperature_logs
+        mkdir $HOME/GPU_temperature_logs
 fi
 
-filename=$HOME/GPU_temperature_logs/${ip}_${datee}_${timee}
+filename=$HOME/GPU_temperature_logs/${all_users}_${datee}_${timee}.txt
 touch $filename
 
-who >> $filename
+echo "logged in users: $all_users" >> $filename
 date >> $filename
 
 echo " " >> $filename
@@ -19,37 +34,52 @@ echo "time | GPU temperature | GPU memory usage" >> $filename
 
 while true
 do
-	temp=`nvidia-smi | grep 'MiB / ' | cut -d ' ' -f 5 | cut -d C -f 1`
-	usage=`nvidia-smi | grep 'MiB / ' | cut -d '|' -f 3 | cut -d '/' -f 1`
+        temp=`nvidia-smi | grep 'MiB / ' | cut -d ' ' -f 5 | cut -d C -f 1`
+        usage=`nvidia-smi | grep 'MiB / ' | cut -d '|' -f 3 | cut -d '/' -f 1`
 
-	echo "GPU temperature: $temp C		Usage: $usage"
+        echo "GPU temperature: $temp C          Usage: $usage"
 
-	timee=`date +%T`
-	echo "$timee | $temp | $usage" >> $filename
+        timee=`date +%T`
+        echo "$timee | $temp | $usage" >> $filename
 
-	if [[ $temp -ge 50 ]]
-	then
-		echo "temperature reached above 50" >> $filename
-		echo "kiling python..." >> $filename
+        if [[ $temp -ge 85 ]]
+        then
+                echo "WARNING!!! temperature reached 85 degree"
+                echo "WARNING!!! temperature reached 85 degree" >> $filename
 
-		id=`nvidia-smi | grep python | cut -d ' ' -f 15`
+                noti -t 'WARNING!!! WARNING!!! WARNING!!!' -m 'GPU temperature is 85 C, training will be stopped at 90 C' --telegram
+                #noti -t 'GPU temperature reached limit' -m 'killed python' -s
 
-		if [ -z "$id" ]
-		then
-			id=`nvidia-smi | grep python | cut -d ' ' -f 16`
-		fi
+        fi
 
-		kill -9 $id
+        if [[ $temp -ge 90 ]]
+        then
+                echo "temperature reached above 90"
+                echo "kiling python..."
 
-		noti -t 'GPU temperature reached limit: $temp C' -m 'killed python'
-		noti -t 'GPU temperature reached limit: $temp C' -m 'killed python' --telegram
-		noti -t 'GPU temperature reached limit' -m 'killed python' -s
+                echo "temperature reached above 90" >> $filename
+                echo "kiling python..." >> $filename
 
-		echo "killed python process" >> $filename
-		echo "done" >> $filename
+                id=`nvidia-smi | grep python | cut -d ' ' -f 15`
 
-		break
-	fi
+                if [ -z "$id" ]
+                then
+                        id=`nvidia-smi | grep python | cut -d ' ' -f 16`
+                fi
 
-	sleep 1
+                kill -9 $id
+
+                #noti -t 'GPU temperature reached limit: $temp C' -m 'killed python'
+                noti -m 'python killed GPU temperature reached limit: 90 C' -t user_$ip --telegram
+                #noti -t 'GPU temperature reached limit' -m 'killed python' -s
+
+                echo "killed python process" >> $filename
+                echo "done" >> $filename
+
+                echo "python killed"
+
+                break
+        fi
+
+        sleep 1
 done
